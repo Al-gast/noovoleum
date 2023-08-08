ddb293e17d6bddb3687c9004a8ac7beca21447f8
Fahmi Sabila Dinnulhaq <fahmisabiladinnulhaq@gmail.com>
2023-08-08 13:34:53 +0700

    nodejs installed and configured

---
+++ app.js
@@ -0,0 +1,19 @@
+const express = require("express");
+const path = require("path");
+const app = express();
+const dotenv = require("dotenv");
+
+dotenv.config(); // Load environment variables from .env
+
+const port = process.env.PORT || 3000; // Default to 3000 if PORT is not defined in .env
+
+app.use("/", express.static(path.join(__dirname, "public")));
+
+app.use((req, res) => {
+  res.status(404);
+  res.sendFile(path.join(__dirname, "public/404.html"));
+});
+
+app.listen(port, () => {
+  console.log(`App listening on port ${port}`);
+});
