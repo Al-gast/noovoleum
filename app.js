ebd440fc1c79056eed0175b7b41b19a7adefba97
Fahmi Sabila Dinnulhaq <fahmisabiladinnulhaq@gmail.com>
2023-08-08 13:50:01 +0700

    fix anchor

--- app.js
+++ app.js
@@ -9,6 +9,8 @@ const port = process.env.PORT || 3000; // Default to 3000 if PORT is not defined
 
 app.use("/", express.static(path.join(__dirname, "public")));
 
+app.use("/blog", express.static(path.join(__dirname, "public/blog-page.html")));
+
 app.use((req, res) => {
   res.status(404);
   res.sendFile(path.join(__dirname, "public/404.html"));
